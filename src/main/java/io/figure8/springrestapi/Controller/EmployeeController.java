38c46a9af735445957954b9985ddee06af731733
Derrick <derrickrlewis@gmail.com>
2021-07-12 02:02:59 -0500

    Initial commit

---
+++ src/main/java/io/figure8/springrestapi/Controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package io.figure8.springrestapi.Controller;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EmployeeController {
+
+    //localhost:8080/employees GET
+    @GetMapping("/employees")
+    public String getEmployees(){
+        return "displaying the list of employees";
+    }
+    @GetMapping("/employees/{id}")
+    public String getEmployee(@PathVariable("id") Long id){
+        return "Fetching the employee details for the id " +id;
+
+    }
+
+
+}
---
+++ src/test/java/io/figure8/springrestapi/SpringrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.figure8.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
